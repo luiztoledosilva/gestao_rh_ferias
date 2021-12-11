ac9cf597b8090dcdfa16ac6f06a457ef743edc69
luiztoledosilva <luiztoledosilva30@gmail.com>
2021-12-11 19:25:07 -0300

    funcionario

---
+++ gestao_rh/funcionarios/models.py
@@ -0,0 +1,14 @@
+
+from django.db import models
+#from .models import Funcionario
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+
+
+# Create your models here.
